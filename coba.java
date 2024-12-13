bcc4564d707101fc4bb8de0dad1ed1f0df655521
iakmorales <zaskiamaulidina12@gmail.com>
2024-12-13 20:27:26 +0700

    Co-authored-by: picha <pichaachuu@users.noreply.github.com>

---
+++ coba.java
@@ -0,0 +1,99 @@
+import java.util.*;
+public class coba {
+    static String[][] dataPelanggan = new String[100][2]; //nama pelanggan, no meja
+    static int[][] dataMENU = new int[100][4]; //kopi hitam, latte, teh tarik, mie goreng
+    static int jumlahPelanggan = 0;
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("===== MENU UTAMA =====");
+            System.out.println("1. Tambahkan Pesanan\n2. Tampilkan Daftar Pesanan\n3. Keluar");
+            System.out.print("Pilih menu: ");
+            int pilihan = sc.nextInt();
+            sc.nextLine();
+            
+            switch (pilihan) {
+                case 1:
+                tambahPesanan(sc);
+                break;
+                case 2:
+                tampildata(sc);
+                break;
+                case 3:
+                System.out.println("Terima kasih telah menggunakan program");
+                return;
+                default:
+                System.out.println("Angka tidak valid. Silahkan input lagi");
+            }
+        }
+    }
+    
+    public static void tambahPesanan (Scanner sc) {
+        int[] hargaMenu = {15000, 22000, 12000, 18000};
+        String[] namaMenu = {"Kopi Hitam", "Latte", "Teh Tarik", "Mie Goreng"};
+        
+        if (jumlahPelanggan < 100) {
+            
+            System.out.print("Masukkan nama pelanggan: ");
+            dataPelanggan[jumlahPelanggan][0] = sc.nextLine();
+            System.out.print("Masukkan nomor meja: ");
+            dataPelanggan[jumlahPelanggan][1] = sc.nextLine();
+
+            System.out.println("===== MENU KAFE =====");
+            for (int i = 0; i < namaMenu.length; i++) {
+                System.out.println((i+1) + ". " + namaMenu[i] + " - Rp " + namaMenu[i]);
+            }
+            
+            while (true) {
+                System.out.print("Pilih menu (masukkan nomor menu, atau 0 untuk selesai): ");
+                int pilihMenu = sc.nextInt();
+
+                if (pilihMenu == 0) {
+                    break;
+                } else if (pilihMenu >= 1 && pilihMenu <=4) {
+                    System.out.print("Masukkan jumlah item untuk " + namaMenu[pilihMenu-1] + ": ");
+                    int jumlahItem = sc.nextInt();
+                    dataMENU[jumlahPelanggan][pilihMenu-1] += jumlahItem;
+                } else {
+                    System.out.println("Pilihan tidak valid, coba lagi.");
+                }
+                
+            }
+            int totalHarga = 0;
+            for (int i = 0; i < 4; i++) {
+                totalHarga += dataMENU[jumlahPelanggan][i] * hargaMenu[i];
+            }
+            System.out.println("Pesanan berhasil ditambahkan.");
+            System.out.println("Total harga pesanan: " + totalHarga);
+            
+            jumlahPelanggan++;
+            
+        } else {
+            System.out.println("Data mahasiswa penuh. Tidak dapat menambahkan data lagi.");
+        }
+
+    }
+    public static void tampildata(Scanner sc) {
+        String[] namaMenu = {"Kopi Hitam", "Latte", "Teh Tarik","Mie Goreng"};
+        int[] hargaMenu = {15000, 22000, 12000, 18000};
+         System.out.println("===== DAFTAR PESANAN =====");
+         for (int i = 0; i < jumlahPelanggan; i++){
+            System.out.println("Nama Pelanggan: " + dataPelanggan[i][0]);
+            System.out.println("Nomor Meja: " + dataPelanggan[i][1]);
+            System.out.println("Detail Pesanan: ");
+            int totalHarga = 0;
+            for (int j = 0; j < namaMenu.length; j++) {
+                int harga = dataMENU[i][j]*hargaMenu[j]; 
+                if (dataMENU[i][j] > 0) {
+                System.out.println("- " + namaMenu[j] + " x " + dataMENU[i][j] + " = " + harga);
+                totalHarga += harga;
+                }
+            }
+            System.out.println("Total Harga Pesanan: " + "Rp " + totalHarga);
+            System.out.println("------------------------------------------------------------");
+        }
+    }
+}
+
--- studycase2.java
+++
@@ -1,7 +0,0 @@
-public class studycase2 {
-
-    public static void main(String[] args) {
-        System.out.println("PICHA");
-        System.out.println("IAK");
-    }
-}
